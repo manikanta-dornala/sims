11b461eb7c443ced0f6c6be8e3175cd73adcf7d5
manikanta-dornala <manikanta-dornala@users.noreply.github.com>
2022-08-01 16:22:39 +0000

    deploy: 7f7f7e028b6928d6c90c4aeb7d1463997d528a8a

--- mandelbrot-set/index.bf32677a.js
+++ mandelbrot-set/index.bf32677a.js
@@ -1,2 +1,2 @@
 let e=new class{constructor(){this.MaxIterations=50}},t=[];!function(a,n,s,i=.01){let o=document.getElementById(a);o&&(o.type="range",o.id=a,o.className="slider",o.value=e[a],o.min=n,o.max=s,o.step=i.toString(),o.addEventListener("mouseup",(function(){window.frames[0].params[a]=parseFloat(o.value)})),t.push(o))}("MaxIterations",0,4*e.MaxIterations);
-//# sourceMappingURL=index.42805b3f.js.map
+//# sourceMappingURL=index.bf32677a.js.map
